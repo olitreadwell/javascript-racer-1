168ae45ad3922c7e51ed82e34f5d843c4b34db73
Oli Treadwell <olitreadwell@gmail.com>
2014-05-19 18:04:00 -0500

    Bindking to player keys and consecutive movement of player token

--- js/application.js
+++ js/application.js
@@ -1,8 +1,19 @@
-function update_player_position(playerNumber, trackPosition){
-    $("#player"+playerNumber+"track :nth-child("+trackPosition+2")").css("background-color", "black");
-};
+function update_player_position(playerNumber, trackPosition) {
+    $("#player" + playerNumber + "track td").removeClass("active");
+    $("#player" + playerNumber + "track :nth-child(" + trackPosition + ")").addClass("active");
+}
 
-$(function(){
-    update_player_position(1,5);
-    update_player_position(2,5);
-});
+$(document).ready(function(){
+
+    $(document).keyup(function(e) {
+        var code = e.keyCode || e.which;
+        if (code == 80) { //Enter keycode
+           console.log("move player 1")
+         } else if (code == 81 ) {
+            console.log("move player 2")
+         }
+    }); // end of keyup
+    // update_player_position(1,5);
+    // update_player_position(2,5);
+
+}); // end of doc ready
