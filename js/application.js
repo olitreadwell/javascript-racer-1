f2bd22d7f4720d3bef07fd730b847c3356540a57
Oli Treadwell <olitreadwell@gmail.com>
2014-05-19 14:17:10 -0500

    first attempts at hard coding an updated player position

--- js/application.js
+++ js/application.js
@@ -1,3 +1,8 @@
-$(function(){
+function update_player_position(playerNumber, trackPosition){
+    $("#player"+playerNumber+"track :nth-child("+trackPosition+2")").css("background-color", "black");
+};
 
+$(function(){
+    update_player_position(1,5);
+    update_player_position(2,5);
 });
